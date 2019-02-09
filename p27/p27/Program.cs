fa189d415c9621235f9211a99289458862a6f5eb
최동환 <nimeol@hotmail.com>
2019-02-09 15:59:32 +0900

    열거형

--- p27/p27/Program.cs
+++ p27/p27/Program.cs
@@ -6,10 +6,33 @@ using System.Threading.Tasks;
 
 namespace p27
 {
-    class Program
+    enum Day1//열거형 = int형 상속
     {
-        static void Main(string[] args)
+        monday,thesday, wednesday, thursday, friday, saturday, sunday
+    }
+    enum Day2 : byte //byte형 상속
+    {
+        monday = 11, thesday, sednesday, thursday, friday, saturday, sunday = monday +100
+    }//숫자 지정 가능
+
+    public class Program
+    {
+        static void Main()
         {
+            Day1 whatDay = Day1.sunday;
+            Console.WriteLine("{0}", whatDay);
+            Console.WriteLine("{0}", (int)whatDay);
+
+            whatDay = (Day1)6; //6을day1 타입으로 출력
+            Console.WriteLine("{0}", whatDay);
+
+            Day2 whatDay2 = Day2.monday;
+            Console.WriteLine("{0}", whatDay2);
+            Console.WriteLine("{0}", (byte)whatDay2);
+
+            whatDay2 = (Day2)111;
+            Console.WriteLine("{0}", whatDay2);
+
         }
     }
 }
