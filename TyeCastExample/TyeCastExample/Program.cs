0eabf770d57d56fa0e397d1cec923b816ab8ab53
최동환 <nimeol@hotmail.com>
2019-02-09 13:12:43 +0900

    test - overflow

---
+++ TyeCastExample/TyeCastExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class TypeCastExample1
+{
+    public static void Main()
+    {
+        short a = 10; //system.int16
+        int b = a; //system.int32
+        //CTS - common type system
+        int c = 50000;
+        try
+        {
+            short d = checked((short)c);//casting 연산 (형변환) -오버플루우
+            Console.WriteLine("Short : {0}", d);
+            Console.WriteLine($"Short : {d}");
+            Console.WriteLine(b);
+            Console.WriteLine($"test : {a},{b}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.StackTrace);
+        }
+    }
+}
