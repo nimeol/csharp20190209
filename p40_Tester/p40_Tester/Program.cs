a9e1a5d019ee7643a486cfe28c8705fa255b26df
최동환 <nimeol@hotmail.com>
2019-02-10 10:00:56 +0900

    선택적 인수

--- p40_Tester/p40_Tester/Program.cs
+++ p40_Tester/p40_Tester/Program.cs
@@ -9,14 +9,19 @@ namespace p40_Tester
     class Tester
     {
         private int m = 88, n = 99;
-
-        Tester(int m = 0, int n = 0)
+        /*
+        Tester() 생성자(method)
+        {
+            this.m = 0;
+            this.n = 0;
+        }
+        */
+        Tester(int m = 0, int n = 0) //null일경우 0으로 대입
         {
             this.m = m;
             this.n = n;
-
         }
-        static int Sum(int i = 0, int n = 0)
+        static int Sum(int i = 0, int j = 0) //매개변수가 없으면 0 으로 대입
         {
             return i + j;
         }
@@ -33,7 +38,7 @@ namespace p40_Tester
             Tester t2 = new Tester(1, 2);
             Console.WriteLine($"m={t2.m}, n{t2.n}");
         }
-    }
+    }/*
     namespace Test
     {
         class Tester
@@ -41,8 +46,15 @@ namespace p40_Tester
             static void Main(string[] args)
             {
                 Console.WriteLine(Minus(1, 2));
-                Console.Writeline(Minus(1));
+                Console.WriteLine(Minus(1));
+                Console.WriteLine(Minus(i: 8, j: 9));
+                Console.WriteLine(Minus(j: 8, i: 9));
+                Console.WriteLine(Minus());
+            }
+            static int Minus(int i = 0, int j = 0)
+            {
+                return i - j;
             }
         }
-    }
+    }*/
 }
