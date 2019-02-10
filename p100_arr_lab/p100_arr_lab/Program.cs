0cbf525f2b35dca9bdc99031a5c794d347b398d3
최동환 <nimeol@hotmail.com>
2019-02-10 17:27:15 +0900

    배열실습

---
+++ p100_arr_lab/p100_arr_lab/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace p100_arr_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] a = { new int[] { 1, 2 }, new int[] { 1, 2, 3 } , new int[] { 1, 2, 3, 4 }};
+
+            int[, ] b = new int[,]{{ 1, 2 }, { 3, 4 },{ 5, 6 } };
+
+        }
+    }
+}
