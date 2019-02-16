f676b66aac98162c9453f78b720984f5364d927e
최동환 <nimeol@hotmail.com>
2019-02-16 14:35:42 +0900

    델리게이트2

---
+++ p147_delegate2/p147_delegate2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+delegate int SumDeli(int i, int j);
+class DeliTest
+{
+    static void Main()
+    {
+        SumDeli s1 = DeliTest.Sum1;
+        SumDeli s2 = new SumDeli(DeliTest.Sum2);
+        S(s1); S(s2);
+    }
+    //아래 메소드는 실제 할일(콜백)이 컴파일 타임에 결정되는 것이 아니라 런타임중에
+    //인자로 넘어오는 델리게이트를 통해 결정된다. 이런 콜백의 구현은 델리게이트로
+    static void S(SumDeli s)
+    {
+        Console.WriteLine(s(1, 2));
+    }
+    static int Sum1(int i, int j)
+    {
+        return i + j;
+    }
+    static int Sum2(int i, int j)
+    {
+        return i + j + 100;
+    }
+}
