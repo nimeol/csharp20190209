37357f3c2804b865086c0f105d76e48bed3a59a7
최동환 <nimeol@hotmail.com>
2019-02-16 16:40:30 +0900

    이벤트

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+    namespace WindowsFormsApp1
+    {
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void button1_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("hello");
+            }
+        }
+        class EventPublisher
+        {
+            //public delegate void MyEventHandler();
+            public event EventHandler MyEvent;
+        public void Do()
+        {
+            //if (MyEvent != null) { MyEvent(); //이벤트 발생 } 아래문장과 같은 의미
+            //MyEvent?.Invoke();
+            if (MyEvent != null)
+            {
+                MyEvent(null, null);
+            }
+        }
+
+
+        }
+        class Subscriber
+        {
+            static void Main(string[] args)
+            {
+                EventPublisher p = new EventPublisher();
+                p.MyEvent += new EventHandler(doAction);
+                p.Do();
+            }
+            static void doAction(object sender, EventArgs e)
+            {
+                Console.WriteLine("MyEvent 이벤트 발생");
+            }
+        }
+
+    }
---
+++ p157_sort/p157_sort/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p157_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
