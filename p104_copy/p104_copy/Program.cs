d617582776b870ae822890e1739fc40e0b388b01
최동환 <nimeol@hotmail.com>
2019-02-16 09:52:29 +0900

    복사(copy)

---
+++ p104_copy/p104_copy/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p104_copy
+{
+    class CreditCard
+    {
+        public string name;
+    }
+}
+
+class CreditCard//참조형 regerance
+{
+    public string name;
+}
+class Customer :ICloneable // "I" 가붙으면 인터페이스 의미
+{
+    public int age;
+    public CreditCard card;//class형 변수
+    public object Clone()//깊은 복사
+    {
+        Customer c = new Customer();
+        c.age = this.age;
+        c.card = new CreditCard();
+        c.card.name = this.card.name;
+        return c;
+    }
+}
+
+class ArrayTest
+{
+    static void Main()
+    {
+        Customer c1 = new Customer();
+        c1.age = 20;
+        c1.card = new CreditCard();
+        c1.card.name = "비씨";
+        Customer c2 = (Customer)c1.Clone();
+        c2.card.name = "bc";
+
+        Console.WriteLine("c1.card.name = " + c1.card.name);
+        Console.WriteLine("c2.card.name = " + c2.card.name);
+    }
+}
+
+
+
+
+/* 얕은복사
+class CreditCard//참조형 regerance
+{
+    public string name;
+}
+class Customer
+{
+    public int age;
+    public CreditCard card;//class형 변수
+    public object ShallowCopy()
+    {
+        return this.MemberwiseClone();//this-> Customer
+        //얕은복사 
+    }
+}
+
+class ArrayTest
+{
+    static void Main()
+    {
+        Customer c1 = new Customer();
+        c1.age = 20;
+        c1.card = new CreditCard();
+        c1.card.name = "비씨";
+        Customer c2 = (Customer)c1.ShallowCopy();
+        c2.card.name = "bc";
+
+        Console.WriteLine("c1.card.name = " + c1.card.name);
+        Console.WriteLine("c2.card.name = " + c2.card.name);
+    }
+}*/
---
+++ test/test/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace test
+{
+    class Emp
+    {
+       public override string//재정의
+       ToString()
+        {
+            return "Emp";
+        }
+    }
+    class Programmer : Emp { }
+    //부모 : emp
+    class Program
+    {
+        static void Main()
+        {
+            Programmer p = new Programmer();
+            Emp e = p as Emp;
+
+            if (e != null)
+                System.Console.WriteLine(e.ToString());
+        }
+    }    
+
+
+    /*
+    class test14
+    {
+        public static void Main()
+        {
+            Console.WriteLine(default(int));
+
+            int isize = sizeof(int);
+            Console.WriteLine($"int : {isize}");
+        }
+    }*/
+    /*
+    class Program
+    {
+        static void Main(string[] args)
+            //args 매개변수 : main 함수로 전달값이 있는 경우
+            //void : return 값이 없는 경우
+            //Main은 하나
+            //static : clr에서 객체 생성없이 바로 메인함수 호출
+        {
+            Console.WriteLine($"hello {args[0]}, {args[1]}");
+        }
+    }
+    */
+    /*
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            short a = 10;
+            int b = a;
+            int c = 50000;
+            try
+            {
+                int d = checked((short)c);// overflow 검사
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"예외 : {e.StackTrace}");
+            }
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+        }
+    }
+    */
+    /*
+    class test11
+    {
+        public const double PI = 3.14; //지역상수를 정의, 초기값 필수
+        public const int MYAGE = 22;
+    }
+
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            double radius = 2;
+            double area = test11.PI * (radius * radius);
+            Console.WriteLine("Area = {0}, Age={1}", area, test11.MYAGE);
+            //test11.MYAGE = 20; //오류
+            const string name = "홍길동";
+            Console.WriteLine("name : " + name);
+            //name = "김길동"; //오류
+        }
+    }*/
+
+}
