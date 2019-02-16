01d7b7023827881d0a3a6ee1daf81f75f6199f3e
최동환 <nimeol@hotmail.com>
2019-02-16 18:18:15 +0900

    람다식

---
+++ 0216_test/0216_test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0216_test
+{
+    delegate void MyDelegate(int i);
+
+    class Eventpublisher
+    {
+        public event MyDelegate DoEvent;
+        public void Do(int num)
+        {
+            if (num % 2 == 0) DoEvent(num);
+        }
+    }
+    class Eventsub
+    {
+        public static void Main()
+        {
+            Eventpublisher p = new Eventpublisher();
+            p.DoEvent += new MyDelegate(Caller);
+            int[] iArr = Array.ConvertAll(
+                (Console.ReadLine()).Split(','),
+                i => int.Parse(i));
+            foreach(int i in iArr)
+            {
+                p.Do(i);
+            }
+        }
+        static void Caller(int num)
+        {
+            Console.WriteLine($"{num} : 짝");
+        }
+
+    }
+    
+}
---
+++ p168_lambda/p168_lambda/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p168_lambda
+{
+    class Program
+    {
+        delegate int calc(int i, int j);
+        
+        static void Main(string[] args)
+        {
+            calc c = new calc(MySum);
+            Console.WriteLine("1+2={0}", c(1, 2));
+            calc c1 = delegate (int i, int j)
+            {
+                return i + j;
+            };
+            Console.WriteLine("3+4={0}", c1(3, 4));
+            calc c2 = (int a, int b) => a + b;
+            Console.WriteLine("3+4={0}", c2(3, 4));
+            calc c3 = (a, b) => a + b;//int 제거 calc 에서 int 가 명시
+            Console.WriteLine("3+4={0}", c2(3, 4));
+            Func<int, int, int> c4 = (a, b) => a + b;
+        }
+        static int MySum(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
