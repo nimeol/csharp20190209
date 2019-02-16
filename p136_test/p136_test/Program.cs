96edd4beea4c89642e8bafbb08bc5450ddac455e
최동환 <nimeol@hotmail.com>
2019-02-16 13:49:22 +0900

    어트리뷰트

---
+++ p136_test/p136_test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+public class Animation
+{
+    private int total; //총 애니메이션 편수
+    private string[] title; //각 볼륨당 제목
+    private string distributor; //유통사
+    private string date; //제작날짜
+    private int price; //가격
+    public Animation(int total, string distributor, string date, int price)
+    {
+        this.total = total; this.title = new string[total];
+        this.distributor = distributor;
+        this.date = date; this.price = price;
+    }
+    //볼륨의 제목 설정
+    public void setTitle(int number, string title) { this.title[number] = title; }
+    //볼륨의 제목을 얻어오는메소드
+    public string getTitle(int number) { return title[number]; }
+    //제작사 정보를 읽어옴
+    public string getDistributor() { return distributor; }
+    //가격 정보를 읽어옴
+    public int getPrice() { return price; }
+    //총애니메이션의 수를 알아내는 메소드
+    public int getTotal() { return total; }
+}
+class AniTest
+{
+    static void Main()
+    {
+        Animation ani = new Animation(5, "한국애니", "10-27-2004", 35000);
+        ani.setTitle(0, "인어공주"); ani.setTitle(1, "신데렐라");
+        ani.setTitle(2, "백설공주"); ani.setTitle(3, "바보온달");
+        ani.setTitle(4, "라이온킹");
+        Console.WriteLine("배급사: {0}", ani.getDistributor());
+        Console.WriteLine("가격: {0}", ani.getPrice());
+        Console.WriteLine("----------------------------");
+        for (int i = 0; i < ani.getTotal(); i++)
+        {
+            Console.WriteLine("Volume{0} : {1}", i, ani.getTitle(i));
+        }
+    }
+}
---
+++ p142_test/p142_test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+[AttributeUsage(AttributeTargets.Class)]//사용처 명시, class에 사용
+public class AdditionalInfoAttribute : Attribute
+{
+    string name; string update; string download;
+
+    public AdditionalInfoAttribute(string name, string update)//생성자, 위치지정파라메타
+        //postional parameter<->명명 parameter
+    {
+        this.name = name; this.update = update;
+    }
+    public string Name { get { return name; } }
+    public string Update { get { return update; } }
+    public string Download
+    {
+        set { download = value; }
+        get { return download; }
+    }
+}
+
+namespace AttributeTest
+{
+    [AdditionalInfoAttribute("홍길동", "2018/10/1", Download = "http://ojc.asia")]
+    class Test
+    {
+        static void Main()
+        {
+            Type type = typeof(Test);
+            foreach (Attribute attr in type.GetCustomAttributes(true))
+            {
+                AdditionalInfoAttribute info = attr as AdditionalInfoAttribute;
+                //as-> 좌측을 우측으로 형변환
+                if (info != null)
+                    Console.WriteLine("Name={0},Update Date={1}," +
+                    "Download ={2}",
+                    info.Name, info.Update, info.Download);
+            }
+        }
+    }
+}
