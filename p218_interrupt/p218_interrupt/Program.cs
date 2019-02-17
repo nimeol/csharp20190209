021dc1abbd13ed322a12e7b95526ba3dad62f2b2
최동환 <nimeol@hotmail.com>
2019-02-17 12:25:49 +0900

    locking

--- p218_interrupt/p218_interrupt/Program.cs
+++ p218_interrupt/p218_interrupt/Program.cs
@@ -7,6 +7,40 @@ using System.Threading;
 
 namespace p218_interrupt
 {
+    public class ThreadTest2
+    {
+        bool sleep = false;
+
+        //차단기가 내려간 상태
+        static AutoResetEvent autoEvent = new AutoResetEvent(false);
+        public void FirstWork()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("F{0}", i);
+                if (i == 5)
+                {
+                    sleep = true;
+                    Console.WriteLine("");
+                    Console.WriteLine("first 쉼...");
+                    autoEvent.WaitOne();
+                }
+            }
+        }
+        public static void Main()
+        {
+            ThreadTest2 t = new ThreadTest2();
+            Thread first = new Thread(new ThreadStart(t.FirstWork));
+            first.Start();
+            while (t.sleep == false) { }
+            Console.WriteLine("first를 깨웁니다...2초후 깨어 납니다.");
+            Thread.Sleep(2000);
+            autoEvent.Set();
+        }
+    }
+
+    /* p218
     class Program
     {
         public static Thread sleeperThread;
@@ -39,4 +73,5 @@ namespace p218_interrupt
             }
         }
     }
+    */
 }
---
+++ p226_multithread/p226_multithread/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+public class ThreadTest3
+{
+    public string lockString = "hello";
+    private object obj = new object();
+    private static Mutex mutex = new Mutex();
+
+    public void Print(string rank)
+    {
+        //lock(obj)
+        //Monitor.Enter(obj);
+        mutex.WaitOne();
+        {
+            for(int i = 0; i<5;i++)
+            {
+                for(int j = 0; j<5;j++)
+                {
+                    Thread.Sleep(100);
+                    Console.Write(",");
+                }
+                Console.WriteLine($"{rank}{lockString}");
+            }
+            //Monitor.Exit(obj);
+            
+        }
+        mutex.ReleaseMutex();
+    }
+    public void FirstWork() { Print("F"); }
+    public void SecondWork() { Print("S"); }
+}
+class TestMaiin
+{
+    [MTAThread]
+    public static void Main()
+    {
+        ThreadTest3 t = new ThreadTest3();
+        Thread first = new Thread(new ThreadStart(t.FirstWork));
+        Thread Second = new Thread(new ThreadStart(t.SecondWork));
+
+        first.Start();
+        Second.Start();
+    }
+}
