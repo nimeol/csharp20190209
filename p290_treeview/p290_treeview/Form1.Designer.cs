621a196fc617bd6fc38e41e4bd26e5a810c807dc
최동환 <nimeol@hotmail.com>
2019-02-23 13:35:52 +0900

    트리리스트

---
+++ p290_treeview/p290_treeview/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace p290_treeview
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FrmMain = new System.Windows.Forms.GroupBox();
+            this.trv1 = new System.Windows.Forms.TreeView();
+            this.lstv1 = new System.Windows.Forms.ListView();
+            this.addRoot = new System.Windows.Forms.Button();
+            this.addChild = new System.Windows.Forms.Button();
+            this.FrmMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.FrmMain.Controls.Add(this.addChild);
+            this.FrmMain.Controls.Add(this.addRoot);
+            this.FrmMain.Controls.Add(this.lstv1);
+            this.FrmMain.Controls.Add(this.trv1);
+            this.FrmMain.Location = new System.Drawing.Point(77, 60);
+            this.FrmMain.Name = "FrmMain";
+            this.FrmMain.Size = new System.Drawing.Size(526, 333);
+            this.FrmMain.TabIndex = 0;
+            this.FrmMain.TabStop = false;
+            this.FrmMain.Text = "Test";
+            // 
+            // trv1
+            // 
+            this.trv1.Location = new System.Drawing.Point(85, 81);
+            this.trv1.Name = "trv1";
+            this.trv1.Size = new System.Drawing.Size(173, 188);
+            this.trv1.TabIndex = 0;
+            // 
+            // lstv1
+            // 
+            this.lstv1.Location = new System.Drawing.Point(321, 81);
+            this.lstv1.Name = "lstv1";
+            this.lstv1.Size = new System.Drawing.Size(178, 188);
+            this.lstv1.TabIndex = 1;
+            this.lstv1.UseCompatibleStateImageBehavior = false;
+            this.lstv1.View = System.Windows.Forms.View.Details;
+            // 
+            // addRoot
+            // 
+            this.addRoot.Location = new System.Drawing.Point(85, 292);
+            this.addRoot.Name = "addRoot";
+            this.addRoot.Size = new System.Drawing.Size(116, 35);
+            this.addRoot.TabIndex = 2;
+            this.addRoot.Text = "루트추가";
+            this.addRoot.UseVisualStyleBackColor = true;
+            this.addRoot.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // addChild
+            // 
+            this.addChild.Location = new System.Drawing.Point(321, 292);
+            this.addChild.Name = "addChild";
+            this.addChild.Size = new System.Drawing.Size(120, 34);
+            this.addChild.TabIndex = 3;
+            this.addChild.Text = "자식추가";
+            this.addChild.UseVisualStyleBackColor = true;
+            this.addChild.Click += new System.EventHandler(this.addChild_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.FrmMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FrmMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox FrmMain;
+        private System.Windows.Forms.Button addChild;
+        private System.Windows.Forms.Button addRoot;
+        private System.Windows.Forms.ListView lstv1;
+        private System.Windows.Forms.TreeView trv1;
+    }
+}
+
---
+++ p290_treeview/p290_treeview/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace p290_treeview
+{
+    public partial class Form1 : Form
+    {
+        Random r = new Random(37);
+        public Form1()
+        {
+            InitializeComponent();
+            lstv1.Columns.Add("Name");
+            lstv1.Columns.Add("Depth");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            trv1.Nodes.Add(r.Next().ToString());
+            TreeToList();
+        }
+
+        private void addChild_Click(object sender, EventArgs e)
+        {
+            if (trv1.SelectedNode == null)
+            {
+                MessageBox.Show("선태된 노드 없음.", "트리뷰테스트",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            trv1.SelectedNode.Nodes.Add(r.Next().ToString());
+            trv1.SelectedNode.Expand();
+            TreeToList();
+        }
+        void TreeToList()
+        {
+            lstv1.Items.Clear();
+            foreach (TreeNode node in trv1.Nodes)
+            {
+                TreeToList(node);
+            }
+        }
+        void TreeToList(TreeNode node)
+        {
+            //TreeNode 형식의 FullPath 속성은 루트노드에서 현재노드까지의 경로를 나타내며
+            //각 경로는 \로 구분한다.
+            lstv1.Items.Add(
+            new ListViewItem(
+            new string[] { node.Text,
+                node.FullPath.Count(f => f == '\\').ToString() }));
+            foreach (TreeNode n in node.Nodes)
+            {
+            }
+            TreeToList(node);
+        }
+    }
+
+}
