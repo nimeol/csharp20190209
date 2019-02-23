6a942fd372f57b75ee6989d867f5ca53b05c430b
최동환 <nimeol@hotmail.com>
2019-02-23 14:58:41 +0900

    ado.net

---
+++ p301_ado.net1/p301_ado.net1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+
+namespace p301_ado.net1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Data source = topcredu;user id = scott; password = tiger";
+            /*string str = @"Data Source=(DESCRIPTION =
+ (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.0.27)(PORT = 1521))
+ (CONNECT_DATA =
+ (SERVER = DEDICATED)
+(SERVICE_NAME = topcredu)
+ )
+ ) ;User Id=scott;Password=tiger";*/
+            OracleConnection Conn = new OracleConnection(str);
+            OracleCommand Comm;
+            Comm = new OracleCommand();
+            Comm.Connection = Conn;
+            try
+            {
+                Conn.Open();
+                Comm.CommandText = "SELECT * FROM EMP";
+                OracleDataReader reader = Comm.ExecuteReader();
+                while(reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(reader.GetOrdinal("ENAME")));
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Conn.Close();
+            }
+        }
+    }
+}
